ecbe08871b88138255783b5a77ac44287320a15c
Karyl Chesman <karylbps@gmail.com>
2025-11-07 21:01:51 -0300

    feat: added a new surface to upscale the content and make looks more like in pixel art

--- game.py
+++ game.py
@@ -11,7 +11,11 @@ class Game:
         pygame.init()
 
         pygame.display.set_caption("Ninja Game")
+
+        # The 'screen' is the actual window, while 'display' is the internal rendering surface,
+        # the idea is to render everything to 'display' and then scale it up to 'screen'
         self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320, 240))
 
         self.clock = pygame.time.Clock()
 
@@ -23,10 +27,10 @@ class Game:
 
     def run(self):
         while True:
-            self.screen.fill((14, 219, 248))
+            self.display.fill((14, 219, 248))
 
             self.player.update((self.movement[1] - self.movement[0], 0))
-            self.player.render(self.screen)
+            self.player.render(self.display)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -42,6 +46,10 @@ class Game:
                         self.movement[0] = False
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
+
+            self.screen.blit(
+                pygame.transform.scale(self.display, self.screen.get_size()), (0, 0)
+            )
             pygame.display.update()
             self.clock.tick(60)
 
