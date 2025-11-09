9fcb2ef4239661b1cfbbc0b51477f3d8b6df7d2e
Karyl Chesman <karylbps@gmail.com>
2025-11-08 22:18:57 -0300

    feat: implemented the tilemap class

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,3 +1,5 @@
+import os
+
 import pygame
 
 BASE_IMG_PATH = "data/images"
@@ -7,3 +9,10 @@ def load_image(path: str) -> pygame.Surface:
     img = pygame.image.load(f"{BASE_IMG_PATH}/{path}").convert()
     img.set_colorkey((0, 0, 0))
     return img
+
+
+def load_images(path: str):
+    images = []
+    for img_path in sorted(os.listdir(f"{BASE_IMG_PATH}/{path}")):
+        images.append(load_image(f"{path}/{img_path}"))
+    return images
