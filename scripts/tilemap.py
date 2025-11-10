1e32c9e4d5e5b005a8e9fc96062b5aaba31aa5cd
Karyl Chesman <karylbps@gmail.com>
2025-11-09 22:36:57 -0300

    feat: added gravity effect to player and a method to check for collisions in tilemap

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -1,10 +1,23 @@
 from __future__ import annotations
+from tabnanny import check
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from game import Game
 
 import pygame
+
+NEIGHBOR_OFFSETS = [
+    (-1, 0),
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+    (1, 0),
+    (0, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
+]
 
 
 class TileMap:
@@ -25,6 +38,17 @@ class TileMap:
                 "variant": 1,
                 "pos": (10, 5 + i),
             }
+
+    def tiles_around(self, pos):
+        tiles = []
+        tile_loc = (int(pos[0] // self.tile_size), int(pos[1] // self.tile_size))
+        for offset in NEIGHBOR_OFFSETS:
+            check_loc = (
+                str(tile_loc[0] + offset[0]) + ";" + str(tile_loc[1] + offset[1])
+            )
+            if check_loc in self.tile_map:
+                tiles.append(self.tile_map[check_loc])
+        return tiles
 
     def render(self, surf: pygame.Surface):
         for tile in self.off_grid_tiles:
